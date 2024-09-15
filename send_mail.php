b3e62b09eb58e8469e7bf0212618faf1d5463ed7
Iuseipadlol <7757595@gmail.com>
2024-09-15 11:15:42 +0300

    Create send_mail.php

---
+++ send_mail.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $message = $_POST['message'];
+
+    // Укажите вашу почту
+    $to = "7757595@gmail.com";
+    $subject = "Новое сообщение с сайта";
+    
+    // Заголовки
+    $headers = "From: noreply@example.com\r
+";
+    $headers .= "Reply-To: noreply@example.com\r
+";
+
+    // Отправка сообщения
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Ваше сообщение было отправлено!";
+    } else {
+        echo "Ошибка при отправке сообщения.";
+    }
+} else {
+    echo "Некорректный запрос.";
+}
+?>
